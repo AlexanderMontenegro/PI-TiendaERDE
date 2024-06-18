7aac683df12dde49fadfaeac09a980f0a4848f73
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-06-18 16:32:19 -0300

    0

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config(); // Carga las variables de entorno desde .env
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres', // Puedes cambiarlo si estás utilizando otro dialecto SQL
-});
-
-module.exports = sequelize;
